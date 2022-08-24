fe06b2c27573325b9279cad5b01877deff8896dc
Eric <wfps4710@gmail.com>
2022-08-24 18:12:21 +0800

    initial commit

---
+++ test/system/friends2s_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class Friends2sTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit friends2s_url
+  #
+  #   assert_selector "h1", text: "Friends2"
+  # end
+end
---
+++ test/system/friends_lists_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class FriendsListsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit friends_lists_url
+  #
+  #   assert_selector "h1", text: "FriendsList"
+  # end
+end
